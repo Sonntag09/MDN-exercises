ab1f5126c63d9ae4a33775491e351717242f057c
Sonntag09 <sonntagcode@gmail.com>
2020-07-14 11:42:31 +0300

    story v1.01

--- javascript/introduction/silly-story-generator/main.js
+++ javascript/introduction/silly-story-generator/main.js
@@ -19,17 +19,23 @@ function result() {
   let xItem = randomValueFromArray(insertX);
   let yItem = randomValueFromArray(insertY);
   let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(/:insertx:/g, xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
   if(customName.value !== '') {
     let name = customName.value;
-
+    newStory = newStory.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
-
+    let weight = Math.round(300 / 14) + ' stone';
+    let temperature =  Math.round(((94 - 32) * 5) / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
